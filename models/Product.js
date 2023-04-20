516b7e67aa5b7059a399825f267f89b879427a59
JoshuaD <joshdadbin@Josh.local>
2023-04-19 18:19:08 -0700

    category routes

--- models/Product.js
+++ models/Product.js
@@ -22,10 +22,17 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
+      validates: {
+        isDecimal: true,
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validates: {
+        isNumeric: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
